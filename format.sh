df27296ce355ce8723ec78ff24be9b772b5dda32
dooly <doolanl208@gmail.com>
2024-12-30 22:24:17 +1000

    this is just the unity package that we are using for liltoon.
    the point of me deleting the existing structure is to
    
    1. have a direct way of being loaded into basis
    2. a way to see where this original came from

--- format.sh
+++
@@ -1,3 +0,0 @@
-dotnet format "./AudioLinkUnityProject/AudioLinkUnityProject.sln" --verbosity diag --severity warn
-dotnet format "./AudioLinkWebProject/AudioLinkWebProject.sln" --verbosity diag --severity warn
-dotnet format "./AudioLinkSandboxUnityProject/AudioLinkSandboxUnityProject.sln" --verbosity diag --severity warn
